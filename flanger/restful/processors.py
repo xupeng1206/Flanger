416cec7fc994774aea899fd947cb9f5f14e60237
xup <peng.xu@quantinfotech.com>
2020-01-20 17:53:28 +0800

    添加一些注释
    Signed-off-by: xup <peng.xu@quantinfotech.com>

--- flanger/restful/processors.py
+++ flanger/restful/processors.py
@@ -21,8 +21,10 @@ class BaseRequestProcessor:
     """
     BaseRequestProcessor: 框架层面的请求处理模块。
 
-    用于处于请求，在这里它会请求的信息，找到对应的resource中的对应的方法，
-    并将对应方法返回的dict和list处理成一个json-response返回出去
+    用于处于请求(类似django的middleware处理request的那部分内容)，在这里它会请求的信息，找到对应的resource中的对应的方法，
+    并将对应方法返回的dict和list处理成一个json-response返回出去。
+    同样它也作为自定义request processor的基类，必须继承与它，自定义基类才有效
+
 
     self.app: 就是flanger的核心对象(flask核心对象+flanger自己的一些东西)
 
@@ -30,9 +32,22 @@ class BaseRequestProcessor:
     """
 
     def __init__(self, app, *args, **kwargs):
+        """
+
+        :param app: 就是flanger的核心对象(flask核心对象+flanger自己的一些东西)
+        :param args:
+        :param kwargs:
+        """
         self.app = app
 
     def process_request(self, request, *args, **kwargs):
+        """
+        process_request
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: response
+        """
         try:
             # 获取url规则
             url_rule = request.url_rule
@@ -77,37 +92,86 @@ class BaseRequestProcessor:
 
 
 class BaseResponseProcessor:
+    """
+    BaseResponseProcessor: 框架层面的response处理模块。
+
+    用于处于response(类似django的middleware处理response的那部分内容), 默认不做啥处理，直接返回
+    写在这里，留一个可能性，也作为自定义response processor的基类，必须继承与它，自定义基类才有效
+
+    self.app: 就是flanger的核心对象(flask核心对象+flanger自己的一些东西)
+
+    process_response: 请求responnse的逻辑部分, 默认框架内啥都没做
+    """
 
     def __init__(self, app, *args, **kwargs):
+        """
+
+        :param app: 就是flanger的核心对象(flask核心对象+flanger自己的一些东西)
+        :param args:
+        :param kwargs:
+        """
         self.app = app
 
     def process_response(self, response):
+        """
+        请求responnse的逻辑部分
+        :param response:
+        :return: response
+        """
         return response
 
 
 class FlangerSwaggerProcessor:
+    """
+    专门用来处理swagger相关的processor
+
+    开发人员想要访问swagger的时候，请求会走到这里，然后去找到swagger对应的resource中的get方法并执行
+    这里swagger的resource和普通的resource有点不一样，这里swagger resource中的get方法返回是一个httpresponse, 而不是字典或list
+    所以拿到返回值后直接返回就行了。
+
+    self.app: 就是flanger的核心对象(flask核心对象+flanger自己的一些东西)
+
+    process_request: 请求处理的逻辑部分
+    """
 
     def __init__(self, app, *args, **kwargs):
+        """
+
+        :param app: 就是flanger的核心对象(flask核心对象+flanger自己的一些东西)
+        :param args:
+        :param kwargs:
+        """
         self.app = app
 
     def process_request(self, request, *args, **kwargs):
+        """
+        process_request  (swagger )
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: http response
+        """
         try:
             url_rule = request.url_rule
             if not url_rule:
                 raise UrlNotFound
 
+            # 在进processor之前，就已经判断过endpoint了, 这里不是重付不必要的操作，直接实例化一个SwaggerResource
             resource = SwaggerResource()
 
+            # 判断一些method, 正常使用的情况下都是get方法，用postman这些工具强行请求post之类的，直接抛一个MethodNotImplement就行
             request_method = request.method.lower()
             method = getattr(resource, request_method, None)
             if method is None:
                 raise MethodNotImplement
 
+            # 抽取参数
             params = {'request': request, 'debug': self.app.config['DEBUG']}
             ret_params = extract_params(request)
             if isinstance(ret_params, dict):
                 params.update(ret_params)
 
+            # 执行SwaggerResource的get方法，内部是个send_file，会得到一个http response，拿到后直接return
             data = method(**params)
             return data
 
